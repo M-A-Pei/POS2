7f02174fa41a9df7d35800cbe18ecbf42853134d
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-11-11 06:25:20 -0800

    added clear cache

---
+++ run-artisan-commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clear and cache configuration
+php artisan config:clear
+php artisan config:cache
+
+# Clear and cache routes
+php artisan route:clear
+php artisan route:cache
+
+# Clear and cache views
+php artisan view:clear
+php artisan view:cache
+
+# Run database migrations
+php artisan migrate --force
